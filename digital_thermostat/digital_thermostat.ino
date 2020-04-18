9120c3792a063563740e04214c7cd1d79a7c7dc4
Tolga Reis <tolga.reis2016@gtu.edu.tr>
2020-04-18 15:09:16 +0300

    project is created

---
+++ digital_thermostat/digital_thermostat.ino
@@ -0,0 +1,14 @@
+#include <ESP8266Wifi.h>
+
+const char *ssid =  "";     // replace with your wifi ssid and wpa2 key
+const char *pass =  "";
+
+WiFiClient client;
+
+void setup() {
+
+}
+
+void loop() {
+
+}
